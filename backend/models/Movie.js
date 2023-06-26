37ff4bf8a1f28921dd67e111a6467bda4c423761
pritanb <pritanbarai@gmail.com>
2023-06-26 19:44:13 +1000

    fixed duplicate movies in list bug

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Schema for movies
 const MovieSchema = new mongoose.Schema({
   user: {type: String, required: true},
-  movie: {type: Object, required: true},
+  movie: {type: Object, required: true, unique: true},
   },
   {timestamps: true}
 );
